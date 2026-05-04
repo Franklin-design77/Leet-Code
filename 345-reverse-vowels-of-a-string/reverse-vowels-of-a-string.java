d57b71f6bf018cd82f360f694868247de67f69bd
Franklin-design77 <franklinphilip60@gmail.com>
2026-05-04 16:47:01 +0530

    Time: 4 ms (39.60%) | Memory: 46.9 MB (25.90%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -23,6 +23,42 @@ class Solution {
     }
     private boolean isVowels(char c){
             return "AEIOUaeiou".indexOf(c)!=-1;
+            //return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
+              
         }
     
 }
+
+
+
+/*class Solution {
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            while (left < right && !isVowel(arr[left])) {
+                left++;
+            }
+            while (left < right && !isVowel(arr[right])) {
+                right--;
+            }
+
+           
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+
+    private boolean isVowel(char c) {
+        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+               c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
+    }
+}
+*/
